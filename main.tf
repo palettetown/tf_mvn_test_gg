bf0bd1e6ce384c7cffcbb3596f164d6021babbec
Chris Yee <kasinyee@outlook.com>
2024-04-01 22:56:18 -0400

    initial

--- main.tf
+++ main.tf
@@ -24,32 +24,32 @@ resource "google_artifact_registry_repository" "docker_repo" {
 }
 
 # Task 3: Create Docker image and upload to the Docker repository
-resource "google_cloudbuild_trigger" "build_trigger" {
-  name        = "docker-image-build-trigger"
-  description = "Trigger to build and push Docker image to Artifact Registry"
-  trigger_template {
-    repo_name   = google_artifact_registry_repository.docker_repo.name
-    branch_name = "main"
-  }
-  filename = "cloudbuild.yaml"
-}
+#resource "google_cloudbuild_trigger" "build_trigger" {
+#  name        = "docker-image-build-trigger"
+#  description = "Trigger to build and push Docker image to Artifact Registry"
+#  trigger_template {
+#    repo_name   = google_artifact_registry_repository.docker_repo.name
+#    branch_name = "main"
+#  }
+#  filename = "cloudbuild.yaml"
+#}
 
-resource "google_project_service" "cloud_run_api" {
-  service = "run.googleapis.com"
-}
+#resource "google_project_service" "cloud_run_api" {
+#  service = "run.googleapis.com"
+#}
 
-resource "google_cloud_run_v2_job" "default" {
-  name     = "cloudrun-job"
-  location = "us-central1"
+#resource "google_cloud_run_v2_job" "default" {
+#  name     = "cloudrun-job"
+#  location = "us-central1"
 
-  template {
-    template {
-      containers {
-        image = "us-central1-docker.pkg.dev/august-water-417802/my-docker-repo/quickstart-image:tag1"
-      }
-    }
-  }
-  depends_on = [ // Here
-    google_project_service.cloud_run_api
-  ]
-}
+#  template {
+#    template {
+#      containers {
+#        image = "us-central1-docker.pkg.dev/august-water-417802/my-docker-repo/quickstart-image:tag1"
+#      }
+#    }
+#  }
+#  depends_on = [ // Here
+#    google_project_service.cloud_run_api
+#  ]
+#}
