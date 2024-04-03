ec7a65653422480d7f55cf3fc6e59e3ddedaeb92
Chris Yee <kasinyee@outlook.com>
2024-04-02 20:30:15 -0400

    initial

--- my_script.sh
+++ my_script.sh
@@ -1,3 +1,1 @@
-gcloud iam service-accounts add-iam-policy-binding "projects/august-water-417802/serviceAccounts/proj1servacc@august-water-417802.iam.gserviceaccount.com" \
-   --project="august-water-417802"   --role="roles/iam.workloadIdentityUser" \
-   --member="principalSet://iam.googleapis.com/projects/198122355685/locations/global/workloadIdentityPools/githubactions/attribute.repository/palettetown/tf_mvn_test_gg"
+gcloud storage buckets create gs://bkt-tf-chrisyue4 --location="US"
